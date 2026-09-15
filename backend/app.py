11c545f07df421121b57b7aadcee9dd186b7238d
aryanjain-codes <aryanjain.codes@gmail.com>
2026-09-15 22:38:21 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -52,4 +52,4 @@ def create_app() -> Flask:
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=False)
